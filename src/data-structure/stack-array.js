a14e5e2ab8a59b7746c5537a9ffc2f700a906249
Tamiris Bonicenha <tamirisapbonicenha@gmail.com>
2022-01-30 23:36:25 -0300

    stack with array

---
+++ src/data-structure/stack-array.js
@@ -0,0 +1,38 @@
+// LIFO - LAST IN FIST OUT
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  toArray() {
+    return this.items;
+  }
+
+  toString() {
+    return this.items.toString();
+  }
+}
